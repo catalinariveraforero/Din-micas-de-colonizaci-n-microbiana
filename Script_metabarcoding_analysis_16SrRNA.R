e232c47f4082f5f7ca4a156ac59ad26fd577c75f
catalinariveraforero <130889877+catalinariveraforero@users.noreply.github.com>
2025-05-20 11:50:34 -0500

    Update Script_metabarcoding_analysis_16SrRNA.R

--- Script_metabarcoding_analysis_16SrRNA.R
+++ Script_metabarcoding_analysis_16SrRNA.R
@@ -1,6 +1,6 @@
 # Author: Catalina Rivera-Forero
 # email: c.riveraf2@uniandes.edu.co | catalina.riveraf@utadeo.edu.co | catalinariveraforero.bm@gmail.com
-#El siguientre script es una modificación del script desarrollado previamente en un estudio del microbioma de Madracis auretenra realizado por Ruiz-Toquica at al. (2025).
+# El siguiente script es una modificación del script desarrollado previamente en un estudio del microbioma de Madracis auretenra realizado por Ruiz-Toquica at al. (2025).
 #Ruiz-Toquica J, Franco Herrera A, Medina M (2025) Endozoicomonas dominance and Vibrionaceae stability underpin resilience in urban coral Madracis auretenra. PeerJ 13:e19226. https://doi.org/10.7717/peerj.19226
 ## Licencia
 # Este proyecto está licenciado bajo la Licencia BSD-3 Clause. Consulte el archivo [LICENSE] para más detalles.
